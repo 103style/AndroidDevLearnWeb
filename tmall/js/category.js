d6115606b3ef9b1ee5d6987f9da4e41236728e82
103style <wtimexiaoke@gmail.com>
2020-12-11 18:08:03 +0800

    menu update

---
+++ tmall/js/category.js
@@ -0,0 +1,86 @@
+class Category {
+
+    constructor(id) {
+        this.box = document.querySelector(id);
+
+        this.categorytab = this.box.querySelector(".category-tab-nav");
+
+        this.tab1 = this.categorytab.querySelector(".tab1");
+        this.tab2 = this.categorytab.querySelector(".tab2");
+        this.menubox = this.box.querySelector(".menu-box");
+        this.menunormal = this.menubox.querySelector(".menu-normal");
+        this.lis = this.menunormal.querySelectorAll("li");
+        this.menudetail = this.menubox.querySelector(".menu-detail");
+        this.menudetailItems = this.menudetail.querySelectorAll(".menu-detail-item");
+        this.menumetting = this.menubox.querySelector(".menu-metting");
+
+        this.timer1 = null
+        this.timer2 = null
+
+        this.init();
+        this.menuListInit();
+    }
+
+    init() {
+        console.log("category")
+
+        this.tab1.addEventListener("mouseenter", (e) => {
+            console.log("tab1 mouseenter")
+            this.removeActive()
+            this.tab1.classList.add("active")
+            this.menunormal.classList.add("active")
+        }, false)
+
+        this.tab2.addEventListener("mouseenter", (e) => {
+            console.log("tab2 mouseenter")
+
+            this.removeActive()
+            this.tab2.classList.add("active")
+            this.menumetting.classList.add("active")
+        }, false)
+    }
+
+
+    menuListInit() {
+        for (var i = 0; i < this.lis.length; i++) {
+            let item = this.lis[i];
+            let index = i;
+            item.addEventListener("mouseenter", (e) => {
+                console.log("li-" + i + " mouseenter")
+                if (this.timer1 != null) {
+                    clearTimeout(this.timer1)
+                }
+                this.timer1 = setTimeout(() => {
+
+                    //先清除menudetail中item的 active
+                    this.menudetailItems.forEach((item) => {
+                        item.classList.remove("active")
+                    })
+
+                    //然后给对应的item加上active
+                    this.menudetailItems[index].classList.add("active")
+                }, 200)
+
+            }, false)
+
+            //当鼠标移出item之后删除 menudetail中对应下标的 item的 active
+            item.addEventListener("mouseleave", (e) => {
+                console.log("li-" + i + " mouseleave")
+                if (this.timer2 != null) {
+                    clearTimeout(this.timer2)
+                }
+                this.timer2 = setTimeout(() => {
+                    this.menudetailItems[index].classList.remove("active")
+                }, 200)
+            }, false)
+        }
+    }
+
+    removeActive() {
+        console.log("remove avtive")
+        this.tab1.classList.remove("active")
+        this.tab2.classList.remove("active")
+        this.menunormal.classList.remove("active")
+        this.menumetting.classList.remove("active")
+    }
+}
